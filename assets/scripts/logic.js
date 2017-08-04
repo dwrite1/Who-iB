0835191c3460f63ccf94a7a0c4e1934cccb92fc2
Jacob Thomas <jcthomas00@gmail.com>
2017-08-04 03:15:21 -0500

    switch to foundation v1

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -1,13 +1,84 @@
-﻿function WhoUB(){
-//	this.sendText = $('#send-text');
+﻿var tempOutput;
+function WhoUB(){
+	//get DOM elements
 	this.sendText = document.getElementById('send-text');
 	this.inputText = $('#input-text');
+	this.loginDiv = $('#logged-out-stuff');
+	this.profileDiv = $('#logged-in-stuff');
+	this.signInButton = document.getElementById('login-button');
+	this.signOutButton = document.getElementById('sign-out');	//TODO
+	this.displayTone = $('#tone-information');
+
+	//add event listeners to DOM elements and bind them to the object's namespace
+	this.signInButton.addEventListener('click', this.signIn.bind(this));
+//	this.signOutButton.addEventListener('click', this.signOut.bind(this));
 
 	this.sendText.addEventListener('click', this.analyzeText.bind(this));
 
+	//firebase initialization
+	this.config = {
+		apiKey: "AIzaSyAq8BljGpg_85mt8OWD4ndWaQGxFnXVpIE",
+		authDomain: "bootcampproject1.firebaseapp.com",
+		databaseURL: "https://bootcampproject1.firebaseio.com",
+		projectId: "bootcampproject1",
+		storageBucket: "bootcampproject1.appspot.com",
+		messagingSenderId: "258703035332"
+	};
+  	firebase.initializeApp(this.config);
+	//get firebase services
+	this.storage = firebase.storage();
+  	this.auth = firebase.auth();	
+	this.database = firebase.database();
+	this.users = 'users/';					//location of all users
+	this.loginDiv.hide();
+	this.profileDiv.hide();
 
+	//called when someone logs in or out
+	this.onAuthStateChanged = function(user) {
+	  if (user) { 							// User is signed in!
+	  	this.loginDiv.hide();					// hide login button
+	  	this.profileDiv.show();
+	    var uid = user.uid; 				// get user info from google auth
+	    var profilePicUrl = user.photoURL;  
+	    var userName = user.displayName;
+	    text = {text1:"heres some text", text2:"heres more text"};
+	  	var userFolder = this.users+uid;	//get the folder for each user
+
+	  	//look for the user based on UID
+	    this.database.ref(this.users+uid).once('value')
+	    	.then(function(snapshot) {
+		    if(snapshot.val()==null){		//User not in DB so add them
+		    	console.log("account doesn't exist");
+			} else {						//user exists, get their info    		
+			    console.log(snapshot.val().userName + " is in our Database");
+			}
+	    });
+		//overrite firbase info
+	    this.database.ref(this.users+uid).set({userName, profilePicUrl, text});
+	    }else{
+	    	this.profileDiv.hide();
+	    	this.loginDiv.show();
+	    }
+	}
+	this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));	//Not sure what this line is about
+	
 }
 
+  //GoogApp method to allow users to sign in
+  WhoUB.prototype.signIn = function(e){
+  		e.preventDefault();
+		var provider = new firebase.auth.GoogleAuthProvider();
+		this.auth.signInWithPopup(provider);    		
+  }
+
+  //GoogApp method to allow users to sign out  
+  WhoUB.prototype.signOut = function(e){
+  	e.preventDefault();
+  	this.auth.signOut();
+  	console.log("signed out");
+  }
+
+//function to take user input and return their sentiment
 WhoUB.prototype.analyzeText = function(){
 	var inputText = this.inputText.val().trim();
 	if (inputText != "") {
@@ -26,11 +97,20 @@ WhoUB.prototype.analyzeText = function(){
 	}
 
 	$.ajax(settings).done(function (response) {
-	  console.log(response);
+			var output = $('<ul>').html($('<li>').html("Sentiment Score: " + 
+			response.documentSentiment.score)).append("Sentiment Magnitude: " + 
+			response.documentSentiment.magnitude);
+		$('#tone-information').html(output);
+	  console.log(response.documentSentiment.magnitude);
+	  console.log(response.documentSentiment.score);
+
 	});	
-		
+
 	}
 
 }
 
-var x = new WhoUB();
+$(document).ready(function(){
+
+	var x = new WhoUB();	
+});
